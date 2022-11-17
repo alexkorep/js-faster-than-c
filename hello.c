372e95aecc6446acbdec9624b6fc5020148f643d
Alexey Korepanov <alexkorep@gmail.com>
2022-11-17 08:32:01 +0100

    Make the code shorter

--- hello.c
+++ hello.c
@@ -1,15 +1,9 @@
-// To compile
-// gcc -Ofast -flto ./hello.c -o hello
-// To profile:
-// time ./hello
-
 #include <stdio.h>
 
+unsigned long long g(void) { return 1; }
+unsigned long long f(void) { return 1; }
 
-unsigned int g(void) { return 1; }
-unsigned int f(void) { return 1; }
-
-unsigned int (*fun_ptr)(void) = &g;
+unsigned long long (*fun_ptr)(void) = &g;
 
 int main(int argc, char **argv)
 {
@@ -18,9 +12,10 @@ int main(int argc, char **argv)
   } else {
     fun_ptr = &f;
   }
-  unsigned int i, j, ret = 0;
+  unsigned long long i, j, ret = 0;
   for (i = 0; i < 40; i++)
     for (j = 0; j < 1000000000U; j++)
       ret += (*fun_ptr)();
+  printf("%llu\n", ret);
   return ret;
 }
--- hello.js
+++ hello.js
@@ -1,17 +1,9 @@
-// To profile:
-// time node ./hello.js
-
-const g = () => {
-  return 1;
-}
-
-const f = () => {
-  return 1;
-}
+const g = () => 1;
+const f = () => 1;
 
 let fun_ptr = g;
 
-if (process.argv.length > 2) {
+if (process.argv.length > 1) {
   fun_ptr = g;
 } else {
   fun_ptr = f;
@@ -21,4 +13,5 @@ let i, j, ret = 0;
 for (i = 0; i < 40; i++)
   for (j = 0; j < 1000000000; j++)
     ret += fun_ptr();
+console.log(ret);
 return ret;
